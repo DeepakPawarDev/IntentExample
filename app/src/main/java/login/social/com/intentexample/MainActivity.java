e379d664c513349f934bbc76b3b54edfd224ed83
Deepak Pawar <pawardeepakv@gmail.com>
2019-06-22 17:23:41 +0530

    Initial commit

---
+++ app/src/main/java/login/social/com/intentexample/MainActivity.java
@@ -0,0 +1,13 @@
+package login.social.com.intentexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
