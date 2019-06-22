a2f1aeb625e2a692b14af2cde2410fbd88be36e7
Deepak Pawar <pawardeepakv@gmail.com>
2019-06-22 18:50:03 +0530

    commit from office

--- app/src/main/java/login/social/com/intentexample/MainActivity.java
+++ app/src/main/java/login/social/com/intentexample/MainActivity.java
@@ -1,16 +1,21 @@
 package login.social.com.intentexample;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final int PICK_IMAGE =100 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +52,36 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https//simfi.in"));
         startActivity(intent);
 
+
+    }
+
+    public void getImage(View view) {
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        intent.putExtra("return-data",true);
+        startActivityForResult(intent,PICK_IMAGE);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==PICK_IMAGE && resultCode==Activity.RESULT_OK){
+
+            Uri uri=data.getData();
+            ((ImageView)findViewById(R.id.image)).setImageURI(uri);
+
+        }
+    }
+
+    public void shareApp(View view) {
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("image/*");
+        startActivity(intent);
+       
 
     }
 }
